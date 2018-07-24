208f5d88e82d4629266e0dba4cd795d6a4795941
Lorena <lorena.giddens@outlook.com>
2018-07-24 20:01:58 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,9 +1,10 @@
 class Costume < ActiveRecord::Base
   def change
     create_table :costumes do |t|
-      t.string :name 
+      t.string :name
       t.integer :price
-      t.integer :size 
+      t.integer :size
       t.text :image_url
     end
+  end
 end
